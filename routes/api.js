e66802162056eeaf906285a4e6ec7ddd7e6a50da
ddyfedd <dzsamboki.techwriter@gmail.com>
2021-04-06 20:16:12 +0200

    GET by id, POST comment

--- routes/api.js
+++ routes/api.js
@@ -72,12 +72,43 @@ module.exports = function (app) {
     .get(function (req, res){
       let bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      Book.findById(bookid, (err, data) => {
+        if(!data) {
+          res.send('no book exist');
+        } else {
+          res.json({
+            _id: data._id,
+            title: data.title,
+            comments: data.comments,
+            commentcount: data.comments.length
+          });
+        }
+      });
     })
     
     .post(function(req, res){
       let bookid = req.params.id;
       let comment = req.body.comment;
       //json res format same as .get
+      if(!comment) {
+        res.send('missing required field comment');
+        return;
+      }
+      Book.findById(bookid, (err, bookdata) => {
+        if(!bookdata) {
+          res.send('no book exist');
+        } else {
+          bookdata.comments.push(comment);
+          bookdata.save((err, saveData) => {
+            res.json({
+              _id: saveData._id,
+              title: saveData.title,
+              comments: saveData.comments,
+              commentcount: saveData.comments.length
+            })
+          });
+        }
+      });
     })
     
     .delete(function(req, res){
